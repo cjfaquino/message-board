c5badccaee2258aeb023dad32fc0d65bb727d214
Christian Aquino <cjfaquino@gmail.com>
2023-03-26 22:43:32 -0700

    add process.env port

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import newRouter from './routes/new';
 import indexRouter from './routes/index';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
